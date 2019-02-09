6c438800fbea15fa02d92c81847c606dedb624f5
Unknown <Karlzone0@gmail.com>
2019-02-09 22:38:42 +0100

    Fixed all of it

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # WeakAura-World-of-Warcraft-WrongEquipmentSet
 
-Works with Battle For Azeroth 8.0.1
+Works with Battle For Azeroth 8.1
 
 A custom lua file for the addon WeakAuras2 of the game World of Warcraft
 It returns text whenever the current equipment set's name does not include the name of the current spec
-As such, in order to use this file, name your equipment sets something like: "Feral", "FeralAoE", "FeralSingleTarget", etc.
+As such, in order to use this file, name your equipment sets something like: "Feral", "AoeFeral", "FeralSingleTarget", etc (this is case-sensitive).
 
-The resulting WeakAura is used for ensuring that you're always in one of the correct equipment sets for your spec, and don't forget to swap, like an idiot (I can neither confirm nor deny that I have forgotten that once or twice).
+The resulting WeakAura is used for ensuring that you're always in one of the correct equipment sets for your spec, and don't forget to swap like an idiot (I can neither confirm nor deny that I have forgotten that once or twice).
--- WeakAuraCurrentEquipmentSetVCurrentSpecialization.lua
+++ WeakAuraCurrentEquipmentSetVCurrentSpecialization.lua
@@ -1,28 +1,47 @@
 
 
 function()
-    local equipedSet = nil
-    for i = 1, C_EquipmentSet.GetNumEquipmentSets() do
-        -- Check all equipment sets and figure out which one is equipped
-        local currentEquipmentSet, _, _, isEquipped = C_EquipmentSet.GetEquipmentSetInfo(i)
-        if isEquipped and currentEquipmentSet ~= nil then
-            equipedSet = currentEquipmentSet
-            break
+    local numSets = C_EquipmentSet.GetNumEquipmentSets()
+
+    if numSets < 1 then
+        return ""
+    end
+
+    local equippedSet = {}
+    local eqSetCount = 0
+    for i = 0, numSets - 1 do
+        -- Check all equipment sets and figure out which ones are equipped (there can be multiple equipped at once)
+        local setName, _, _, isEquipped = C_EquipmentSet.GetEquipmentSetInfo(i)
+        if isEquipped then
+            eqSetCount = eqSetCount + 1
+            equippedSet[eqSetCount] = setName -- Arrays start at one
         end
     end
     
-    if equipedSet == nil then
+    if eqSetCount == 0 then -- Arrays start at one in LUA
         return "Currently no set equipped"
-        
-    else
-        local currentSpecID = GetSpecializationInfo(GetSpecialization())
-        local currentSpecName = GetSpecializationNameForSpecID(currentSpecID)
-        if not string.find(equipedSet, currentSpecName) then 
-            return "Wrong equipment set: " .. equipedSet .. "For spec: " .. currentSpecName
-            -- Returns here if it cannot find the current spec name inside the equipmentSetName
-        else 
-            return "" 
-        end 
     end
+    
+    local currentSpecID = GetSpecializationInfo(GetSpecialization())
+    local currentSpecName = GetSpecializationNameForSpecID(currentSpecID)
+    -- If one of the equipped sets contains the spec name, then we're good
+    for i = 1, eqSetCount do
+        if string.find(equippedSet[i], currentSpecName) then 
+            return "" 
+        end
+    end
+
+    -- If neither of our equipped set contains the spec name, then we have the wrong set equipped
+    if eqSetCount == 1 then
+        return "Wrong equipment set: " .. equippedSet[1] .. "\nFor spec: " .. currentSpecName
+    end
+
+    local wrongSets =  equippedSet[1]
+    for i = 2, eqSetCount do
+        wrongSets = wrongSets .. ", " .. equippedSet[i]
+    end
+    return "Wrong equipment sets: " .. wrongSets .. "\nFor spec: " .. currentSpecName
 end
 
+-- To dump all equipment set info to chat for debugging purposes, do this:
+-- /run for i=0,C_EquipmentSet.GetNumEquipmentSets() do print( C_EquipmentSet.GetEquipmentSetInfo(i) ) end
